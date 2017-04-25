0f5588d50b6a49b955bb532929104d3a252f5050
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-25 11:52:32 +0100

    added erb and views

--- hello.rb
+++ hello.rb
@@ -1,22 +1,20 @@
 require 'sinatra'
 require 'sinatra/reloader' if development?
 
-get '/' do
-  "hello!"
-end
-
-get '/secret' do
-  "This is a secret route"
-end
-
-get '/russian' do
-  'p90 rush b!'
-end
+# get '/' do
+#   "hello!"
+# end
+#
+# get '/secret' do
+#   "This is a secret route"
+# end
+#
+# get '/russian' do
+#   'p90 rush b!'
+# end
 
 get '/cat' do
-  "<div style='border:4px dashed red'>
-    <img src='/images/cat_picture.png'>
-  </div>"
+  erb(:index)
 end
 
 # get '/:name' do
